4251ae7f843cc89be5e1ea82aa82952e902f2ff3
Srikanth Racharla <srikanth@Srikanths-MacBook-Pro.local>
2022-06-17 14:51:54 +0530

    sidebar functionaliry added

--- final/js/app.js
+++ final/js/app.js
@@ -1,1 +1,15 @@
-console.log("Hello");
+// sidebar functionality
+(function () {
+  const hamburger = document.querySelector(".hamburger");
+  const sidebarCloseButton = document.querySelector(".close-button");
+  const sidebarContainer = document.querySelector(".sidebar-container");
+
+  const openSidebar = function () {
+    sidebarContainer.classList.add("show-sidebar");
+  };
+  const closeSidebar = function () {
+    sidebarContainer.classList.remove("show-sidebar");
+  };
+  hamburger.addEventListener("click", openSidebar);
+  sidebarCloseButton.addEventListener("click", closeSidebar);
+})();
