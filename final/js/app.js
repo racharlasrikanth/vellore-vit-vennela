179c53de9ba794c246f420baaf8d2c024ad53422
Srikanth Racharla <srikanth@Srikanths-MacBook-Pro.local>
2022-06-23 17:56:56 +0530

    desktop accessability checks fixed

--- final/js/app.js
+++ final/js/app.js
@@ -4,14 +4,28 @@
   const sidebarCloseButton = document.querySelector(".close-button");
   const sidebarContainer = document.querySelector(".sidebar-container");
 
+  const updateAccessiblityChecks = function (sidebarOpening) {
+    const sidebarLinks = [
+      ...document.querySelectorAll(".sidebar-links .nav-link"),
+    ];
+    sidebarLinks.forEach((eachLink) => {
+      eachLink.setAttribute("tabIndex", sidebarOpening ? 0 : -1);
+    });
+    document
+      .querySelector(".sidebar-container .logo-section a")
+      .setAttribute("tabIndex", sidebarOpening ? 0 : -1);
+  };
   const openSidebar = function () {
     sidebarContainer.classList.add("show-sidebar");
+    updateAccessiblityChecks(true);
   };
   const closeSidebar = function () {
     sidebarContainer.classList.remove("show-sidebar");
+    updateAccessiblityChecks(false);
   };
   hamburger.addEventListener("click", openSidebar);
   sidebarCloseButton.addEventListener("click", closeSidebar);
+  updateAccessiblityChecks(false);
 })();
 
 // sticky sidebar
